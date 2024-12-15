099b2962a2d8bf1dbc5442d8276c27b984626696
Arda Doğrul <darda159@gmail.com>
2024-12-15 14:30:56 +0300

    Creating Example Space

--- Search Algorithm A New Mathematical Approach/headers/Library.hpp
+++ Search Algorithm A New Mathematical Approach/headers/Library.hpp
@@ -4,18 +4,24 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <cctype>
+#include <filesystem>
+using namespace std;
+
 
 class ASCIIMeanSearch {
 private:
     int _StringLength;
     int _meanOfString;
     char* _string;
-    std::string _fileDirectory;
 
 public:
 
-    ASCIIMeanSearch(int maxLength = 0, const std::string& fileDirectory = "")
-        : _StringLength(maxLength), _meanOfString(0), _fileDirectory(fileDirectory) {
+    ASCIIMeanSearch(int maxLength = 0)
+        : _StringLength(maxLength), _meanOfString(0){
         _string = new char[_StringLength + 1]();
     }
 
@@ -29,6 +35,16 @@ public:
         return _StringLength;
     }
 
+        int get_file_size(const std::string& _directory){
+        /*(TR) İmlecin konumunu döndürüp dosya boyutunu bulunur.
+        (EN) Returns the position of the cursor and finds the file size.
+        */
+        std::ifstream file(_directory, std::ios::in | std::ios::binary);
+        file.seekg(0, std::ios::end); 
+        int fileSize = file.tellg();
+        return fileSize;
+    }
+
     void setString(const char* str) {
         if (str) {
             size_t length = std::min(strlen(str), static_cast<size_t>(_StringLength));
@@ -37,7 +53,7 @@ public:
         }
     }
 
-    int calculateMean() {
+    int calculateMean(char* _string) {
         if (!_string || strlen(_string) == 0) {
             return 0;
         }
@@ -49,6 +65,40 @@ public:
         _meanOfString = sum / length;
         return _meanOfString;
     }
+
+std::vector<std::string> takeWords(const std::string& _fileDirectory) {
+    std::vector<std::string> _words;
+    ifstream file(_fileDirectory);      
+    string line;
+
+    if (file.is_open()) {
+        
+        while (std::getline(file, line)) {
+            stringstream ss(line);
+            string word;
+
+            while (ss >> word) {
+                word.erase(
+                    std::remove_if(
+                    word.begin(), word.end(), [](char c) {
+                        return std::ispunct(static_cast<unsigned char>(c));}),
+                        word.end());
+                if (!word.empty()) {
+                    _words.push_back(word);
+                }
+            }
+        }
+
+        file.close();
+    } else {
+        std::cerr << "Dosya açılamadı: " << _fileDirectory << std::endl;
+    }
+
+    return _words;
+}
+
+
+    //search()
 
 };
 
--- Search Algorithm A New Mathematical Approach/main/main.cpp
+++ Search Algorithm A New Mathematical Approach/main/main.cpp
@@ -1,8 +1,29 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <string>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    /*
+        ASCIIMeanSearch asciiMeanSearch;
+        string fileDirectory = "C:\\Users\\User\\Desktop\\test.txt";
+        string word = "test";
+        std::vector<std::string> words;
+        asciiMeanSearch.stringLength(word);
+        asciiMeanSearch.calculateMean(word);
+        words = asciiMeanSearch.takeWords(fileDirectory);
+        int size = words.size();
+         int* _searchSpaceMean = new int[size];
+        for(auto word : words){
+            _searchSpaceMean.push_back(asciiMeanSearch.calculateMean((char*)word.c_str()));
+        }
+
+
+
+    */
+
+
     return 0;
 }
